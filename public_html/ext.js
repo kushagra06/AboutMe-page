55e01e853637a647f55730edc5fa1e26378039a4
Kushagra Chandak <kushagrachandak06@gmail.com>
2016-02-21 18:38:16 +0530

    AboutMe html pages

---
+++ public_html/ext.js
@@ -0,0 +1,7 @@
+var days=['Mon', 'Tues', 'Wed' ];
+days[3]='Thurs';
+document.write(days[0]+'<br>');
+document.write("<b>Length= </b>"+ days.length +"<br>");
+document.write("Reversed= " +days.reverse()+"<br>");
+document.write("Sorted= " +days.sort()+"<br>");
+alert("This is JS called externally");
